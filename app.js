9eb6de1ca1caeba751f82434bd7ff7b49c80f132
falr95 <frodega70@gmail.com>
2017-03-08 20:00:17 -0300

    armo este branch para pasar laburo de windors a ubuntu

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var path = require('path');
 var handlebars = require('express-handlebars');
 var mongoClient = require('mongodb').MongoClient;
 
@@ -13,17 +14,15 @@ mongoClient.connect('mongodb://ds157549.mlab.com:57549/pulsion', function(err,db
 		db.close();
 	} else {
 		console.log('Conexión a la base de datos establecida');
+		console.log(process.env.MONGOUSER, process.env.MONGOPASS);
 		db.authenticate(process.env.MONGOUSER, process.env.MONGOPASS, function(err, result) {
 			if (result == true) {
 				console.log('Autenticación con la base de datos exitosa');
-
 				module.exports.db = db;
-
-				app.engine('handlebars', handlebars());
-				app.set('view engine', 'handlebars');
-
+				app.set('views', path.join(__dirname, 'views'));
+				app.set('view engine', 'hbs');
+				app.use(express.static(path.join(__dirname, 'public')));
 				app.get('/', indexController);
-
 				app.listen(3000, function() {
 					console.log('Escuchando en el puerto 3000');
 				});
@@ -35,5 +34,4 @@ mongoClient.connect('mongodb://ds157549.mlab.com:57549/pulsion', function(err,db
 		});
 	}
 });
-
 
